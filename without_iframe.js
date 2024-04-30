ff06d4a16d2364152d6311801890aadba02e219c
Arun8072 <atunviknezh999@gmail.com>
2024-04-30 20:02:00 +0530

    edit 12

--- without_iframe.js
+++ without_iframe.js
@@ -1,5 +1,5 @@
-var link_pelement,category_txts,crumbs,w_now,s_now,p;
-var w,x;
+var link_pelement,category_txts,crumbs,w_now,s_now,read_btn,media;
+var w,x,p;
 //var l,h,k,j,m,o,s;
 details = { };
 //worked - looped through all weeks
@@ -9,9 +9,9 @@ console.log(weeks);
 
 async function ck() {
 
-for ( w = weeks.length-1 ; w>=0 ; w--) {
+for ( w = weeks.length-1; w>=0 ; w--) {
 weeks[w].click();
-await new Promise(r => setTimeout(r,5000));
+await new Promise(r => setTimeout(r,2500));
 
   console.log("weeks "+ w);
    // weeks[j].childNodes[0].click();
@@ -43,15 +43,18 @@ console.log(details);
 
 details["link00"].click();
 //selecting the section from the same week opened, does change the whole html so it stays in the page so you can continue inspect element
+solve();
+async function solve() {
 
-await new Promise(r => setTimeout(r,2000));
+
+await new Promise(r => setTimeout(r,8000));
 
 crumbs=document.getElementsByClassName('breadcrumb-item');
 w_now=crumbs[1].textContent.match(/\d+/);
 w_now=w_now[0]-1; //-1 to start from zero index
 s_now=crumbs[2].textContent;
 //console.log(w_now);
-console.log(s_now);
+//console.log(s_now);
 
 for (p = 0;p < category_txts.length;p++) {
   if(s_now==details["name"+w_now+p]){
@@ -62,15 +65,35 @@ for (p = 0;p < category_txts.length;p++) {
 console.log(p);
 console.log(details["name"+w_now+p]);
 
-    if(details["category"+w_now+p]=="Video"){
-      console.log(details["category"+w_now+p]);
-      return;
-    }else if(details["category"+w_now+p]=="Reading"){
-      console.log(details["category"+w_now+p]);
-    }else if(details["category"+w_now+p]=="Quiz"){
-      console.log(details["category"+w_now+p]);
-    }//if
+next_btn=document.querySelector("a[aria-label='Next Item']");
 
+    if(details["category"+w_now+p]=="Video"){
+      console.log("Video");
+       media = document.querySelector("video");
+       console.log(media);
+       media.play();
+     await new Promise(r => setTimeout(r,1000));
+      //The HTMLMediaElement API is used for video controls
+      // time is calculated in seconds
+console.log(media.currentTime);
+console.log(media.duration);
+      media.currentTime = Math.floor(media.duration);
+      console.log("Video played");
+await new Promise(r => setTimeout(r,500));
+      next_btn.click();
+      solve();
+    }else if(details["category"+w_now+p]=="Reading"){
+      console.log("Reading");
+       read_btn = document.querySelector('button[data-testid="mark-complete"]');
+       read_btn.click();
+       console.log("Completed reading");
+       next_btn.click();
+       solve();
+    }else {
+      console.log("other");
+      next_btn.click();
+    }//if
+}//func-solve
 }//fun
 ck();
 
